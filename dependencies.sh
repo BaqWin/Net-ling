44e2e3a81474ddac9fbcff7d8172a5fa28919e44
Marcin <marcinjagielak90@gmail.com>
2024-08-29 16:06:07 +0200

    Added gtest to dependencies script

--- dependencies.sh
+++ dependencies.sh
@@ -2,6 +2,18 @@
 sudo apt update
 sudo apt install -y git cmake make g++ libpcap-dev libssl-dev python3 python3-pip python3-scapy
 
+if [ ! -d "googletest" ]; then
+  git clone https://github.com/google/googletest.git
+fi
+
+cd googletest
+mkdir -p build
+cd build
+cmake ..
+make
+sudo make install
+cd ../..
+
 if [ ! -d "PcapPlusPlus" ]; then
   git clone https://github.com/seladb/PcapPlusPlus.git
 fi
