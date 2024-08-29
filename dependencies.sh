248415117fc0a42210e7b1cc75574de211c345e3
Marcin <marcinjagielak90@gmail.com>
2024-08-29 15:57:05 +0200

    Changes to dependencies install script file

--- dependencies.sh
+++ dependencies.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y git cmake make g++ libpcap-dev python3 python3-pip
+sudo apt install -y git cmake make g++ libpcap-dev libssl-dev python3 python3-pip python3-scapy
 
 if [ ! -d "PcapPlusPlus" ]; then
   git clone https://github.com/seladb/PcapPlusPlus.git
 fi
-cd PcapPlusPlus
-./configure-linux.sh
-make all
-sudo make install
 
-pip3 install scapy
+cd PcapPlusPlus
+mkdir -p build
+cd build
+cmake ..
+make
+sudo make install
